535468233f863704ad058bdebc3c4c3cde0794ec
Souradeep <a3000206@adelaide.edu.au>
2026-03-18 20:24:31 -0700

    Add Card abstract base class

---
+++ DeadMansDraw/Card.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <vector>
+#include <string>
+
+enum CardType { Cannon, Chest , Key, Anchor, Sword, Hook, Oracle, Map, Mermaid, Kraken };
+
+class Player; 
+class Game; 
+
+class Card {
+    protected:
+        CardType _type;
+        int _value;
+
+    public: 
+        Card(CardType type, int value) : _type(type), _value(value) {}
+
+        virtual ~Card() {}
+
+        const CardType& type() const {
+            return _type; 
+        }
+
+        int value() const {
+            return _value; 
+        }
+
+        virtual std::string str() const = 0; 
+
+        virtual void play(Game& game, Player& player) = 0; 
+
+        virtual void willAddToBank(Game& game, Player& player) {}
+};
+
+typedef std::vector<Card*> CardCollection; 
