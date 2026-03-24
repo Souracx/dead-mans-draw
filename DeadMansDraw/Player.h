4df89b97da04457cf004dbaefe5e89e289936b43
Souradeep <a3000206@adelaide.edu.au>
2026-03-23 22:09:50 -0700

    Add Player header

---
+++ DeadMansDraw/Player.h
@@ -0,0 +1,30 @@
+#pragma once 
+#include "Card.h"
+#include <iostream>
+
+class Game; 
+
+class Player {
+private: 
+    std::string _name; 
+    CardCollection _playArea; 
+    CardCollection _bank; 
+
+public: 
+    Player(); 
+    ~Player(); 
+
+    //Adds card to play area, returns true if bust 
+    bool playCard(Card* card, Game& game); 
+
+    void bankCards(Game& game); //moves play area to bank 
+    int score() const; //return total score of bank 
+    bool isBust() const; //return true if play area has duplicate suit 
+
+    void printPlayArea() const; //prints play are to screen 
+    void printBank() const; //prints bank to screen 
+
+    std::string getName() const; //returns player's name 
+    CardCollection& getBank(); //returns reference to bank 
+    CardCollection& getPlayArea(); //returns reference to play area
+};
