0f912e66d4ab6a75e57e4cd78c8b7d67bf3909b7
Souradeep <a3000206@adelaide.edu.au>
2026-04-02 00:50:53 -0700

    Implement OracleCard play ability

--- DeadMansDraw/OracleCard.cpp
+++ DeadMansDraw/OracleCard.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include "OracleCard.h"
+#include "Player.h"
+#include "Game.h"
 
 std::string OracleCard::str() const {
     return "Oracle(" + std::to_string(_value) + ")"; 
 }
 
-void OracleCard::play(Game& game, Player& player){ }
+//peak at top of card deck before deciding to draw
+void OracleCard::play(Game& game, Player& player){
+    CardCollection& deck = game.getDeck(); 
+    if (deck.empty()) {
+        std::cout << " The Oracle sees nothing - the deck is empty.\n"; 
+        return;
+    }
+    //Peek at top
+    Card* top = deck.back(); 
+    std::cout << "The Oracle sees a " << top->str() << "\n";
+}
