3c9c4bfc9c1f1eac1983e350adc5867140a91f81
Souradeep <a3000206@adelaide.edu.au>
2026-04-01 23:26:18 -0700

    Implement MermaidCard play message

--- DeadMansDraw/MermaidCard.cpp
+++ DeadMansDraw/MermaidCard.cpp
@@ -5,5 +5,7 @@ std::string MermaidCard::str() const {
     return "Mermaid(" + std::to_string(_value) + ")"; 
 }
 
-void MermaidCard::play(Game &game, Player& player) {}
+void MermaidCard::play(Game &game, Player& player) {
+    std::cout << " No effect but Mermaids are worth more.\n"; 
+}
 
