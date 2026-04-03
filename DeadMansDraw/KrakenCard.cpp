b7ef9c260bc679a78b811ffa3b8e78e8fcb1362b
Souradeep <a3000206@adelaide.edu.au>
2026-04-03 00:03:48 -0700

    Implement KrakenCard play ability

--- DeadMansDraw/KrakenCard.cpp
+++ DeadMansDraw/KrakenCard.cpp
@@ -1,10 +1,30 @@
 #include "KrakenCard.h"
 #include <iostream>
+#include "Game.h"
+#include "Player.h"
 
 std::string KrakenCard::str() const {
     return "Kraken(" + std::to_string(_value) + ")"; 
 }
 
+//Ability - must draw 3 cards consecutively 
 void KrakenCard::play(Game& game, Player& player) {
-    //todo 
+    std::cout << "Draw 3 cards from the deck and play each: \n"; 
+
+    for (int i = 0; i < 3; i++) {
+        if (game.getDeck().empty()) {
+            std::cout << "Deck is empty - Kraken stops.\n"; 
+            break; 
+        }
+        Card* card = game.drawCard(); 
+        std::cout << player.getName() << " draws a " << card->str() << "\n"; 
+
+        bool bust = player.playCard(card, game); 
+        if (bust) {
+            std::cout << "BUST! " << player.getName() << " loses all cards in play area\n"; 
+            player.discardPlayArea(game); 
+            return; //turn is over
+        }
+
+    }
 }
