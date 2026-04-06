fc512b054f5c0b63171c511a3769465f2c588a0a
Souradeep <a3000206@adelaide.edu.au>
2026-04-06 12:10:38 -0700

    Implement play() ability for KeyCard

--- DeadMansDraw/KeyCard.cpp
+++ DeadMansDraw/KeyCard.cpp
@@ -5,6 +5,8 @@ std::string KeyCard::str() const {
     return "Key(" + std::to_string(_value) + ")"; 
 }
 
-void KeyCard::play(Game& game, Player& player) {}
-
+void KeyCard::play(Game& game, Player& player) {
+    std::cout << " No immediate effect. If banked with chest, draw as many bonus cards from the Discard pile as you moved into your Bank.\n"; 
+}
+//Chest hand's the ability 
 void KeyCard::willAddToBank(Game& game, Player& player) {}
