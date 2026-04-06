dbc88aac5e05ecdd9384de6909b44e65e595d4f4
Souradeep <a3000206@adelaide.edu.au>
2026-04-05 19:19:36 -0700

    Add pseudcode for HookCard and SwordCard

--- DeadMansDraw/HookCard.cpp
+++ DeadMansDraw/HookCard.cpp
@@ -1,10 +1,20 @@
 #include "HookCard.h"
 #include <iostream>
+#include "Game.h"
+#include "Player.h" 
 
 std::string HookCard::str() const {
     return "Hook(" + std::to_string(_value) + ")";
 }
 
+//Ability - Can use top card from own bank 
 void HookCard::play(Game& game, Player& player) {
-    //todo 
+    //pseudocode 
+    //1. Get own bank 
+    //2. if bank empty print and return 
+    //3. find highest card per suit in own bank 
+    //4. Display numbered list of choice 
+    //5. Get player's choice 
+    //6. Remove chosen card from own bank 
+    //7. play the card into play area(check for bust) 
 }
--- DeadMansDraw/SwordCard.cpp
+++ DeadMansDraw/SwordCard.cpp
@@ -5,4 +5,13 @@ std::string SwordCard::str() const {
     return "Sword(" + std::to_string(_value) + ")";
 }
 
-void SwordCard::play(Game& game, Player& player) {}
+void SwordCard::play(Game& game, Player& player) {
+    //pseudocode - similar to hook but opponent 
+    //1. Get opponent and the bank 
+    //2. Check for empty bank - print and return 
+    //3. Find highest card per suit in opponents' bank 
+    //4. Display choice 
+    //5. Get player's choice 
+    //6. Remove chosen card from opponent's bank 
+    //7. Play stlen card into play area ( can bust ) 
+}
