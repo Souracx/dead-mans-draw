c83281596d684c62759cb886f4381ea954855db0
Souradeep <a3000206@adelaide.edu.au>
2026-04-07 14:39:15 -0700

    Add AnchorCard header file: Bonus Task

---
+++ DeadMansDraw/AnchorCard.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Card.h"
+
+class AnchorCard : public Card {
+public: 
+    AnchorCard(int value) : Card(Anchor, value) {
+    }
+
+    virtual std::string str() const override; 
+
+    virtual void play(Game& game, Player& player) override; 
+};
